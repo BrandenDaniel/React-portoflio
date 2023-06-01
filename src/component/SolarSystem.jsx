0dcf13372501a5c21ebb52f006cc004376f04499
Branden Ng <brandendanielng@gmail.com>
2023-06-01 23:57:17 +1000

    style enhacnement

--- src/component/SolarSystem.jsx
+++ src/component/SolarSystem.jsx
@@ -1,15 +1,18 @@
 export default function SolarSystem() {
   return (
-    <div className="solar-system">
-      <div className="solar-system__sun"></div>
-      <div className="solar-system__mercury"></div>
-      <div className="solar-system__venus"></div>
-      <div className="solar-system__earth"></div>
-      <div className="solar-system__mars"></div>
-      <div className="solar-system__jupiter"></div>
-      <div className="solar-system__saturn"></div>
-      <div className="solar-system__uranus"></div>
-      <div className="solar-system__neptune"></div>
-    </div>
+    <>
+      <div className="solar-system-forefront"></div>
+      <div className="solar-system">
+        <div className="solar-system__sun"></div>
+        <div className="solar-system__mercury"></div>
+        <div className="solar-system__venus"></div>
+        <div className="solar-system__earth"></div>
+        <div className="solar-system__mars"></div>
+        <div className="solar-system__jupiter"></div>
+        <div className="solar-system__saturn"></div>
+        <div className="solar-system__uranus"></div>
+        <div className="solar-system__neptune"></div>
+      </div>
+    </>
   );
 }
