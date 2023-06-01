ca6cfff33ed875de4e55d51d0aee4bb7425281ea
Branden Ng <brandendanielng@gmail.com>
2023-06-01 21:28:29 +1000

    style enhancements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,10 +31,8 @@ function Home() {
     <div className="home">
       <div className="home__header">
         <h1>
-          A front-end developer with a profound <span></span> for translating
-          creativity into captivating &nbsp;
-          <span></span>&nbsp;
-          <span></span>
+          A front-end developer with a profound <span></span> <span></span>{" "}
+          translating creativity into captivating <span></span> <span></span>
         </h1>
         <div className="home__external-links">
           <div>
